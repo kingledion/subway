db93ba0446d4e7879dd5d1838f8c9a697cf89cb6
Dan Hartig <kingledion@gmail.com>
2017-05-09 09:53:29 -0400

    Starting independent respository for thesis

---
+++ build_stations.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+
+import subway_utils as su, csv , numpy as np, networkx as nx, re
+from rtree import index
+from math import cos, radians, pi
+import shapely.geometry as shpgeo, shapefile, pandas as pd
+
+db, cursor = su.opendb()
+
+# Station class to use in conjunction with the rtree index
+class station:
+    
+    def __init__(self, lat, lon, datadict = {}):
+        self._data = datadict
+        self._data['lat'] = float(lat)
+        self._data['lon'] = float(lon)
+        
+    def __getitem__(self, key):
+        if key not in self._data:
+            raise IndexError(key)
+        return self._data[key]
+        
+    def __setitem__(self, key, value):
+        self._data[key] = value
+        
+    def __hash__(self):
+        return hash((self._data['lat'], self._data['lon']))
+        
+    def __iter__(self):
+        for key in self._data:
+            yield key
+        
+    def __eq__(self, other):
+        return True if self._data['lat'] == other._data['lat'] and  self._data['lon'] == other._data['lon'] else False
+        
+    def getfields(self):
+        return [k for k in self._data]
+        
+    def getDataDict(self): # Inelegant solution!
+        return self._data
+
+def near_station(station, idx, pts):
+    return [next(idx.nearest((*p, *p), 1, objects='raw')) == station for p in pts]   
+    
+def in_shape(state, pts):
+    return [state.contains(shpgeo.Point(*p)) for p in pts]
+    
+def build_station_index(filename):
+    with open(filename, 'r') as csvin:
+        rdr = csv.reader(csvin, delimiter = ';', quotechar = "'")
+        
+        idx = index.Index()
+        stations = []
+        
+        for row in rdr:
+            if len(row) == 4:
+                d = su.est_density(cursor, float(row[2]), float(row[1]))
+                s = station(row[1], row[2], {**d, **{'name': row[0]}, **{'parking': row[3]}})     
+                stations.append(s)
+                idx.insert(0, (s['lon'], s['lat'], s['lon'], s['lat']), s)
+                
+            elif row:
+                print(row)
+                            
+    return stations, idx
+       
+def calculate_areas(stations, idx, shp):
+    # calculate latitude degrees to 1 km
+    ns_deg = 1.0/110.574
+
+    for s in stations:
+        n = 100 # done 1000
+        # calculate lon degrees to 1km
+        ew_deg = 1.0/111.320/cos(radians(s['lat']))
+    
+        Theta = np.random.randn(n, 2)
+        R = np.sqrt(np.random.rand(n))
+        vectors = Theta * np.stack([R*ew_deg / np.linalg.norm(Theta, axis=1), R*ns_deg / np.linalg.norm(Theta, axis=1)], axis=1)
+        
+        # pts in a 1km area
+        pts = [(v[0] + s['lon'], v[1] + s['lat']) for v in vectors]
+        walk = [1 if a else 0 for a in in_shape(shp, pts)]
+        wcount = sum(walk)
+        near = sum(1 if a and b else 0 for a, b in zip(near_station(s, idx, pts), walk))
+
+        s['narea'] = near*pi/n # area of a 1km radius circle is pi
+        s['warea'] = wcount*pi/n
+        
+        # pts in 15km area
+        pts = [(v[0]*15 + s['lon'], v[1]*15 + s['lat']) for v in vectors]
+        drive = sum(1 if a and b else 0 for a, b in zip(near_station(s, idx, pts), in_shape(shp, pts)))
+        s['darea'] = drive*225*pi/n
+        
+        print(s['name'], "{0:.2f}".format(wcount*pi/n), "{0:.2f}".format(near*pi/n), "{0:.2f}".format(drive*225*pi/n))
+        
+#def calculate_totals(stations):
+#    fields = stations[0].getfields()
+#    dfields = [f for f in fields if f.endswith('density')]
+#    for s in stations:
+#        for d in dfields:
+#            name = d.split('density')[0]
+#            
+#            s[name + 'near'] = int(s[d] * s['narea'])
+#            s[name + 'walk'] = int(s[d] * s['warea'])
+#            s[name + 'drive'] = int(s[d] * s['darea'])
+            
+def to_dataframe(stations):
+    list_of_dict = [s.getDataDict() for s in stations] # There must be a better way!
+    df = pd.DataFrame(list_of_dict)
+    dfields = [f for f in list(df) if f.endswith('density')]
+    
+    for d in dfields:
+        name = d.split('density')[0]
+        df[name + 'near'] = df[d] * df['narea']
+        df[name + 'walk'] = df[d] * df['warea']
+        df[name + 'drive'] = df[d] * df['darea']
+
+    return df
+    
+    
+            
+def loadnetwork(filename):
+    G = nx.MultiDiGraph()    
+    
+    with open(filename, 'r') as csvin:
+        rdr = csv.reader(csvin, delimiter = ';', quotechar = "'")
+        
+        for row in rdr:
+            if len(row) == 5:
+                src_l, src_s, tar_l, tar_s, w = row
+                if not tar_l: 
+                    # This means stations on the same line. Add both directions
+                    # on the directed graph and  src_l = tar_l.
+                    #print([(src_l + src_s, src_l + tar_s), (src_l + tar_s, src_l + src_s)])
+                    G.add_edge(src_l + ":" + src_s, src_l + ":" + tar_s, weight = float(w))
+                    G.add_edge(src_l + ":" + tar_s, src_l + ":" + src_s, weight = float(w))
+                    #G.add_edges_from([(src_l + src_s, src_l + tar_s), (src_l + tar_s, tar_l + src_s)], weight=w)
+                elif src_s == tar_s:
+                    # This means transfer between lines on the same station
+                    # Add only this one direction (other should be defined separate)
+                    G.add_edge(src_l + ":" + src_s, tar_l + ":" + tar_s, weight = float(w))
+                
+            elif row:
+                print(row)
+                
+    return G
+    
+def get_nearby_nodes(G, r, names):
+    graphs = [nx.ego_graph(G, x, radius = r, distance='weight') for x in names]
+    g =  graphs[0] if len(graphs) <= 1 else nx.compose_all(graphs) # should I be using compose or union?
+    return set([x.split(":")[1] for x in g.nodes()])
+    
+            
+def add_station_network(df, G):
+    stations = df['name'] 
+    
+    dfields = [f for f in list(df) if f.endswith('near')]
+    nfields = [(d, d.split("near")[0] + 'net') for d in dfields]
+    
+    for s in stations:
+        nodenames = [x for x in G.nodes() if x.split(":")[1] == s]
+
+        within15 = get_nearby_nodes(G, 15, nodenames)
+        within30 = get_nearby_nodes(G, 30, nodenames)
+        within60 = get_nearby_nodes(G, 60, nodenames)
+        
+        for old, new in nfields:
+            
+            df['15' + new] = df.loc[df['name'].isin(within15), old].sum()
+            df['30' + new] = df.loc[df['name'].isin(within30), old].sum()
+            df['60' + new] = df.loc[df['name'].isin(within60), old].sum()   
+            
+#def write_stations(stations, filename):
+#
+#    with open(filename , 'w') as outfile:
+#        wrtr = csv.writer(outfile) 
+#        wrtr.writerow(fields)
+#        for s in stations:
+#            wrtr.writerow([s[f] for f in fields])
+
+################################################
+# Load station geo data; build station lists; build station geoindices
+# Load station network
+    
+bstations, bidx = build_station_index('/opt/school/stat672/subway/boston_subwaygeo.csv')
+cstations, cidx = build_station_index('/opt/school/stat672/subway/chicago_subwaygeo.csv')
+print("Density estimate and index built")
+
+# load station network maps
+bnetwork = loadnetwork('/opt/school/stat672/subway/boston_network.csv')
+cnetwork = loadnetwork('/opt/school/stat672/subway/chicago_network.csv')
+
+#############################################
+# Load shapefiles, calculate available walking (1km) and driving (15km) areas
+    
+# load shapefile of states in question (index 32 = MA, 19 = IL)
+sf = shapefile.Reader('./shapes/cb_2015_us_state_20m')
+
+calculate_areas(bstations, bidx, shpgeo.shape(sf.shape(32).__geo_interface__))
+calculate_areas(cstations, cidx, shpgeo.shape(sf.shape(19).__geo_interface__))
+print("Areas calculated")
+
+###############################################
+# Convert to datafram and multiply densities by areas to get counts
+
+bframe = to_dataframe(bstations)
+cframe = to_dataframe(cstations)
+print("Converted to dataframes")
+
+##############################################
+# Add nearby stations on the network to station data
+
+add_station_network(bframe, bnetwork)
+add_station_network(cframe, cnetwork)
+print("Station network data added")
+
+        
+###############################################
+# Write staions to csv files
+        
+bframe.to_csv("/opt/school/stat672/subway/boston_stations.csv") 
+cframe.to_csv("/opt/school/stat672/subway/chicago_stations.csv")     
+           
+#write_stations(bstations, "/opt/school/stat672/subway/boston_stations.csv")    
+#write_stations(cstations, "/opt/school/stat672/subway/chicago_stations.csv")  
---
+++ regression.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 26 20:30:19 2017
+
+@author: dhartig
+"""
+
+import pandas as pd, numpy as np, warnings
+import sklearn.linear_model as lm, sklearn.svm as svm
+
+def std_features(X, mean, std):
+    X = X - mean
+    with warnings.catch_warnings():
+        warnings.filterwarnings('error')
+        try:
+            X = X/std
+        except Warning as warn:
+            X = np.nan_to_num(X)
+    return(X)
+
+# Read in data; merge into two datasets
+with open("/opt/school/stat672/subway/boston_stations.csv") as csvin:
+    sdata = pd.read_csv(csvin)
+with open('/opt/school/stat672/subway/boston_subway_ridership.csv') as csvin:
+    rdata = pd.read_csv(csvin, delimiter = ';', quotechar = "'", names = ['name', 'riders'])
+data1 = pd.merge(sdata, rdata, how = 'inner', on='name')
+    
+with open("/opt/school/stat672/subway/chicago_stations.csv") as csvin:
+    sdata = pd.read_csv(csvin)  
+with open('/opt/school/stat672/subway/chicago_subway_ridership.csv') as csvin:
+    rdata = pd.read_csv(csvin,  delimiter = ';', quotechar = "'", names = ['name', 'riders'])  
+data2 = pd.merge(sdata, rdata, how = 'inner', on='name')
+    
+# Select columns for use in regression
+fields = ['popnear', 'housenear', 'empnear', 'paynear', 'popwalk', 'housewalk', 'empwalk', 'paywalk', 'popdrive', 'housedrive', 'parking',
+          '15empnet','30empnet','60empnet','15housenet','30housenet','60housenet','15paynet','30paynet','60paynet','15popnet','30popnet','60popnet']
+X1 = data1.as_matrix(columns=fields)
+X2 = data2.as_matrix(columns=fields)
+
+# Standardize X values
+xmn, xst = np.mean(X1, axis=0), np.std(X1, axis=0)
+X1std = std_features(X1, xmn, xst)
+X2std = std_features(X2, xmn, xst)
+
+y1 = np.ravel(data1.as_matrix(columns=['riders']))
+y2 = np.ravel(data2.as_matrix(columns=['riders']))
+
+# Standardize y vlues
+ymn, yst = np.mean(y1), np.std(y1)
+y1std = std_features(y1, ymn, yst)
+y2std = std_features(y2, ymn, yst)
+
+def scoreLsq(Xtrain, ytrain, Xtest, ytest):
+
+    coeff, resid, rank, s = np.linalg.lstsq(Xtrain, ytrain)
+    
+    predicted = np.dot(Xtrain, coeff)
+    sstot = sum((ytrain - np.ones(ytrain.shape)*np.mean(ytrain))**2)
+    ssres = sum((ytrain - predicted)**2)
+    sc1 = 1-ssres/sstot
+    
+    predicted = np.dot(Xtest, coeff)
+    sstot = sum((ytest - np.ones(ytest.shape)*np.mean(ytest))**2)
+    ssres = sum((ytest - predicted)**2)
+    sc2 = 1-ssres/sstot
+    #print(["{0:.2f}".format(x) for x in coeff])
+    return (sc1, sc2)
+    
+def scorePolyLsq(Xtrain, ytrain, Xtest, ytest):
+    pass
+    
+def scoreRBFSVR(c, Xtrain, ytrain, Xtest, ytest):
+    model = svm.SVR(kernel='rbf', C = c)
+    model.fit(Xtrain, ytrain)
+    #print(len(ytrain), len(model.support_))
+    return (model.score(Xtrain, ytrain), model.score(Xtest, ytest))
+
+def scoreRidge(a, Xtrain, ytrain, Xtest, ytest):
+    model = lm.Ridge(alpha = a, fit_intercept = False, solver='svd')    
+    model.fit(Xtrain, ytrain)   
+    #print(["{0:.2f}".format(x) for x in model.coef_])
+    return (model.score(Xtrain, ytrain), model.score(Xtest, ytest))
+        
+def scoreLASSO(a, Xtrain, ytrain, Xtest, ytest):
+    model = lm.Lasso(alpha = a, fit_intercept=False, precompute=True, max_iter=100000)
+    model.fit(Xtrain, ytrain)   
+    #print(["{0:.2f}".format(x) for x in model.coef_])
+    return (model.score(Xtrain, ytrain), model.score(Xtest, ytest))
+    
+
+print('\nLinear Least Squares Regression')   
+print("Train=Boston; Test=Chicago: {0:.3f}, {1:.3f}".format(*scoreLsq(X1, y1, X2, y2)))
+print("Train=Chicago; Test=Boston: {0:.3f}, {1:.3f}".format(*scoreLsq(X2, y2, X1, y1)))
+
+for c in [1.5]:
+    print('\nRadial Basis Function SVR (C = {0})'.format(c))
+    print("Train=Boston; Test=Chicago: {0:.3f}, {1:.3f}".format(*scoreRBFSVR(c, X1std, y1std, X2std, y2std)))
+    print("Train=Chicago; Test=Boston: {0:.3f}, {1:.3f}".format(*scoreRBFSVR(c, X2std, y2std, X1std, y1std)))
+
+for a in [40]:
+    print('\nRidge Regression (alpha = {0})'.format(a))
+    print("Train=Boston; Test=Chicago: {0:.3f}, {1:.3f}".format(*scoreRidge(a, X1std, y1std, X2std, y2std)))
+    print("Train=Chicago; Test=Boston: {0:.3f}, {1:.3f}".format(*scoreRidge(a, X2std, y2std, X1std, y1std)))
+
+for a in [8]:
+    print('\nLASSO Regression (alpha = {0})'.format(a))
+    print("Train=Boston; Test=Chicago: {0:.3f}, {1:.3f}".format(*scoreLASSO(a, X1std, y1std, X2std, y2std)))
+    print("Train=Chicago; Test=Boston: {0:.3f}, {1:.3f}".format(*scoreLASSO(a, X2std, y2std, X1std, y1std)))
